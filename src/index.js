77a019a0ab6c739e5f8a9ae916f79bfd27fea485
federico rodriguez <tafedericoemanuel@gmail.com>
2024-04-25 18:36:44 -0300

    8

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { CalendarApp } from './CalendarApp.js';
+
+
 import "./style.css";
+
+console.log(process.env)
 
 
 ReactDOM.render(
